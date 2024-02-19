ff79c92ea3678ce040914533226a1dcc7ee6ed10
Venkata Kousik <Venkatakousikcse01@gmail.com>
2024-02-19 06:25:54 +0530

    hello

--- client/src/components/player/Statistics/leftContent/BarGraph.jsx
+++ client/src/components/player/Statistics/leftContent/BarGraph.jsx
@@ -4,62 +4,62 @@ function Statistics() {
         <div className="simple-bar-chart">
 
             <div className="item" style={{ '--clr': '#963D97', '--val': 80 }}>
-                <div className="label">Label 1</div>
+                <div className="label">Jan</div>
                 <div className="value">80%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#FCB72A', '--val': 50 }}>
-                <div className="label">Label 2</div>
+                <div className="label">Feb</div>
                 <div className="value">50%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#F8821A', '--val': 100 }}>
-                <div className="label">Label 3</div>
+                <div className="label">Mar</div>
                 <div className="value">100%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#FCB72A', '--val': 15 }}>
-                <div className="label">Label 4</div>
+                <div className="label">Apr</div>
                 <div className="value">15%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#963D97', '--val': 1 }}>
-                <div className="label">Label 5</div>
+                <div className="label">May</div>
                 <div className="value">1%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#069CDB', '--val': 90 }}>
-                <div className="label">Label 6</div>
+                <div className="label">June</div>
                 <div className="value">90%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#963D97', '--val': 80 }}>
-                <div className="label">Label 1</div>
+                <div className="label">July</div>
                 <div className="value">80%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#FCB72A', '--val': 50 }}>
-                <div className="label">Label 2</div>
+                <div className="label">Aug</div>
                 <div className="value">50%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#F8821A', '--val': 100 }}>
-                <div className="label">Label 3</div>
+                <div className="label">Sep</div>
                 <div className="value">100%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#E0393E', '--val': 15 }}>
-                <div className="label">Label 4</div>
+                <div className="label">Oct</div>
                 <div className="value">15%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#963D97', '--val': 1 }}>
-                <div className="label">Label 5</div>
+                <div className="label">Nov</div>
                 <div className="value">1%</div>
             </div>
 
             <div className="item" style={{ '--clr': '#069CDB', '--val': 90 }}>
-                <div className="label">Label 6</div>
+                <div className="label">Dec</div>
                 <div className="value">90%</div>
             </div>
         </div>
